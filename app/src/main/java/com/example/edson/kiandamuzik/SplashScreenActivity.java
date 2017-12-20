bdf69f71ab50fe80e6e7d1a0a6daf33022c567e5
Tatt13 <tatianaoliveiravilarinho@outlook.com>
2017-12-20 16:08:15 +0100

    Implement SplashScreen Activity

--- app/src/main/java/com/example/edson/kiandamuzik/SplashScreenActivity.java
+++ app/src/main/java/com/example/edson/kiandamuzik/SplashScreenActivity.java
@@ -3,11 +3,27 @@ package com.example.edson.kiandamuzik;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        };
+        task.run();
     }
 }
